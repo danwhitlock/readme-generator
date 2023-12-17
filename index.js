90ad5448903f5184ae2d77667e0f0f6bcc8f229c
danwhitlock <110103983+danwhitlock@users.noreply.github.com>
2023-12-17 11:50:06 +0000

    create questions array

--- index.js
+++ index.js
@@ -5,7 +5,15 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+"Please enter your github username",
+"Please enter your email address",
+"What is the title of this project?",
+"Please enter a short description of the project",
+"What license applies to this project?",
+"How should any dependencies be installed?",
+"How should any tests be run?",
+"Please enter any special usage instructions for users",
+"What does the user need to know about contributing to the repo?"
 ];
 
 // function to write README file
